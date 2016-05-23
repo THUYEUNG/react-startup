646e123faf04d8e57c43468630f918b788109df4
YeungYeung <yeungyeung@yeungyeungs-mbp.attlocal.net>
2016-05-23 01:40:53 -0500

    First commit

---
+++ src/components/Test.jsx
@@ -0,0 +1,10 @@
+var React = require('react');
+var Test = React.createClass({
+    render: function() {
+        return (
+          <h1> This is the test </h1>
+        );
+    }
+});
+
+module.exports = Test;
---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Test = require('./components/Test.jsx');
+
+ReactDOM.render(<Test />, document.getElementById('test'));
